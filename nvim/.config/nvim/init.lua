5a94b4be10369288aba2944621b3af4ebe2aed57
TekuNut <13456422+TekuNut@users.noreply.github.com>
2025-02-08 22:33:07 +0000

    Remove nvim-minifiles and configure godot integration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Start nvim server
+local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
+if not vim.uv.fs_stat(pipepath) then
+  vim.fn.serverstart(pipepath)
+end
+
+-- Setup LSP
+local lspconfig = require("lspconfig")
+lspconfig.gdscript.setup({})
+
+-- Configure file type extensions
+vim.filetype.add({
+  pattern = {
+    ["*.gd"] = "gdscript",
+  },
+})
