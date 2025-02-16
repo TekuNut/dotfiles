2ab188a1ab2951ac283d502dc61c50da98538441
TekuNut <13456422+TekuNut@users.noreply.github.com>
2025-02-16 19:11:45 +0000

    Move customization into options.lua

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,3 +5,23 @@
 --- LSP Server to use for Python.
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
+
+-- Neovide config
+if vim.g.neovide then
+  -- Disable animations
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0.00
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_scroll_animation_length = 0.00
+end
+
+-- [[ Godot configuration ]]
+-- Start nvim server if a project.godot file is detected.
+local godot_projectfile = vim.fn.getcwd() .. "/project.godot"
+local godot_socket_path = vim.fn.stdpath("run") .. "/godot.pipe"
+if godot_projectfile and not vim.uv.fs_stat(godot_socket_path) then
+  vim.fn.serverstart(godot_socket_path)
+end
