db51d1e4f0d1298c304527e7284d1577ee163acc
TekuNut <13456422+TekuNut@users.noreply.github.com>
2025-02-16 18:31:42 +0000

    Add mini.surround

--- nvim/.config/nvim/lua/plugins/extend-nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/extend-nvim-treesitter.lua
@@ -1,9 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- Install Godot related parsers
-    table.insert(opts.ensure_installed, "gdscript")
-    table.insert(opts.ensure_installed, "godot_resource")
-    table.insert(opts.ensure_installed, "gdshader")
-  end,
+  opts = {
+    ensure_installed = {
+      -- Godot
+      "gdscript",
+      "godot_resource",
+      "gdshader",
+    },
+  },
 }
