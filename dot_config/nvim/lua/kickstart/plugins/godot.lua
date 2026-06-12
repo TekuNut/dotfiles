2493f2c4c42141f4ae4cd18a10e888b6ab06c7a8
TekuNut <13456422+TekuNut@users.noreply.github.com>
2026-06-12 12:52:20 +0100

    Use local leader for godot-related bindings

--- dot_config/nvim/lua/kickstart/plugins/godot.lua
+++ dot_config/nvim/lua/kickstart/plugins/godot.lua
@@ -1,10 +1,10 @@
 return {
-  'Mathijs-Bakker/godotdev.nvim',
-  dependencies = { 'nvim-dap', 'nvim-dap-ui', 'nvim-treesitter' },
-  opts = {
-    csharp = true,
-  },
-  keys = {
-    { '<leader>Gk', '<cmd>GodotDocs<cr>', desc = 'Godot Docs' },
-  },
+	"Mathijs-Bakker/godotdev.nvim",
+	dependencies = { "nvim-dap", "nvim-dap-ui", "nvim-treesitter" },
+	opts = {
+		csharp = true,
+	},
+	keys = {
+		{ "<LocalLeader>Gk", "<cmd>GodotDocs<cr>", desc = "Godot Docs" },
+	},
 }
