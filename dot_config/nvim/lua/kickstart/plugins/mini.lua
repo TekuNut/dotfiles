8488def534d57eb7d510a3f53270828183be6148
TekuNut <13456422+TekuNut@users.noreply.github.com>
2026-06-12 15:11:36 +0100

    Use mini.files instead of oil

--- dot_config/nvim/lua/kickstart/plugins/mini.lua
+++ dot_config/nvim/lua/kickstart/plugins/mini.lua
@@ -1,32 +1,43 @@
 return {
-  { -- Collection of various small independent plugins/modules
-    'nvim-mini/mini.nvim',
-    lazy = false,
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      -- Move selection in any direction
-      -- <M-h> - Move line left
-      -- <M-l> - Move line right
-      -- <M-j> - Move line down
-      -- <M-k> - Move line up
-      require('mini.move').setup()
-
-      require('mini.icons').setup()
+  -- Better Around/Inside textobjects
+  --
+  -- Examples:
+  --  - va)  - [V]isually select [A]round [)]paren
+  --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+  --  - ci'  - [C]hange [I]nside [']quote
+  {
+    'nvim-mini/mini.ai',
+    opts = { n_lines = 500 },
+  },
+  -- Add/delete/replace surroundings (brackets, quotes, etc.)
+  --
+  -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+  -- - sd'   - [S]urround [D]elete [']quotes
+  -- - sr)'  - [S]urround [R]eplace [)] [']
+  {
+    'nvim-mini/mini.surround',
+    opts = {},
+  },
+  {
+    'echasnovski/mini.icons',
+    opts = {},
+    lazy = true,
+    specs = {
+      { 'nvim-tree/nvim-web-devicons', enabled = false, optional = true },
+    },
+    init = function()
+      package.preload['nvim-web-devicons'] = function()
+        require('mini.icons').mock_nvim_web_devicons()
+        return package.loaded['nvim-web-devicons']
+      end
     end,
+  },
+  {
+    'nvim-mini/mini.files',
+    opts = {},
+    dependencies = {
+      'mini.icons',
+    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
