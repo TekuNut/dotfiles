5a7ee6fa72f7b209dab3d5ab5ca5c4cd8baf5624
TekuNut <13456422+TekuNut@users.noreply.github.com>
2026-06-12 12:22:37 +0100

    Add binding for C++

---
+++ dot_config/nvim/after/ftplugin/cpp.lua
@@ -0,0 +1,3 @@
+# C++ specific bindings
+vim.keymap.set("n", "<LocalLeader>a", "<Cmd>LspClangdSwitchSourceHeader<CR>", { buffer = true })
+vim.keymap.set("n", "<LocalLeader>cs", "<Cmd>LspClangdShowSymbolInfo<CR>", { buffer = true })
