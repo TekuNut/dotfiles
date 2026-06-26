7d404b0f90db097e6c559c6714caf9b376884f8d
TekuNut <13456422+TekuNut@users.noreply.github.com>
2026-06-26 13:07:49 +0100

    Remove old nvim config and add zshrc

--- dot_config/nvim-pack/after/ftplugin/cpp.lua
+++
@@ -1,3 +0,0 @@
-# C++ specific bindings
-vim.keymap.set("n", "<LocalLeader>ca", "<Cmd>LspClangdSwitchSourceHeader<CR>", { buffer = true })
-vim.keymap.set("n", "<LocalLeader>cs", "<Cmd>LspClangdShowSymbolInfo<CR>", { buffer = true })
